0dc1e7c9d62177aad6ecf78fcddeaf39e62f8de6
josias <josias.nanez@mi.unc.edu.ar>
2025-05-23 02:32:39 -0300

    arquitectura MVP y base de datos simplificada

--- src/SatisPlanning/componentes/componente_mover.py
+++ src/SatisPlanning/componentes/componente_mover.py
@@ -1,6 +1,6 @@
 import pygame
 import SatisPlanning.constantes as ct
-from SatisPlanning.componente import Componente
+from SatisPlanning.componentes.componente import Componente
 
 class ComponenteMover(Componente):
     def __init__(self, personaje, componente_animacion):
